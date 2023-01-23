63bcd2589b876d0a4436db9ee7f5a19fef3ccab7
PoweredDeveloper <powered.developer@gmail.com>
2023-01-24 00:12:01 +0300

    Count Reset

--- src/App.jsx
+++ src/App.jsx
@@ -57,7 +57,7 @@ function App() {
       guarantees: { lastUnique: 0, lastLegendary: 0, lastEpic: 0, lastRare: 0 },
     })
     setCharacters([])
-    setGachagems(10000000)
+    setGachagems(10000)
   }
 
   const sendCharacters = (character) => {

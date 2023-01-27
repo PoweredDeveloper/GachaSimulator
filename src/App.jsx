e61b5aedfd3f10d3769b6e4d04b585406b9d6e3d
PoweredDeveloper <powered.developer@gmail.com>
2023-01-27 22:28:01 +0300

    🛠️ - Some Fixes

--- src/App.jsx
+++ src/App.jsx
@@ -57,7 +57,7 @@ function App() {
       guarantees: { lastUnique: 0, lastLegendary: 0, lastEpic: 0, lastRare: 0 },
     })
     setCharacters([])
-    setGachagems(1000)
+    setGachagems(1000e10)
   }
 
   const sendCharacters = (character) => {
